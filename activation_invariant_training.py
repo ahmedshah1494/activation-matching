e8b817c9c81267e0c5909ee170bfd904f551cdb6
ahmed <shinigami1494@github.com>
2020-09-07 15:23:26 -0400

    moved adversarial invariance trainer to different file

--- activation_invariant_training.py
+++ activation_invariant_training.py
@@ -4,6 +4,7 @@ from torch import nn
 from torch.utils.data import DataLoader, Subset
 import torchvision
 from trainer import Trainer, compute_accuracy
+from activation_invariance_trainer import ActivationInvarianceTrainer, AdamMoM, SGDMoM
 from attack_classifier import extract_attack
 from models import LayeredModel, VGG16, WideResnet, GaussianNoiseLayer
 from copy import deepcopy
@@ -11,171 +12,26 @@ import argparse
 from utils import get_cifar10_dataset
 import os
 from trades import trades_loss
-
-def logit_cross_entropy(logits_p, logits_q, T=1):
-    p = torch.softmax(logits_p/T, dim=1)
-    q = torch.log_softmax(logits_q/T, dim=1)
-    return torch.mean(torch.sum(-p*q, dim=1))
-
-class ActivationInvarianceTrainer(Trainer):
-    def __init__(self, model:LayeredModel, train_loader, val_loader, test_loader, optimizer, scheduler, device, args):
-        super(ActivationInvarianceTrainer, self).__init__(model, train_loader, val_loader, test_loader, optimizer, scheduler, device, args)                
-        attack_class, kwargs = extract_attack(args)
-        self.training_adversary = self.test_adversary = attack_class(self.model, **kwargs)
-        if self.args.maximize_logit_divergence:
-            attack_class, kwargs = extract_attack(args, loss_fn=logit_cross_entropy)
-            self.training_adversary = attack_class(self.model, **kwargs)
-
-    def compute_outputs(self, x, y, xadv=None):
-        model = self.model
-        logits, interm_Z = model(x, store_intermediate=True)
-        if xadv is None:            
-            if model.training:
-                model.requires_grad_(False)
-                model = model.train(False)
-                if self.args.maximize_logit_divergence:                    
-                    xadv = self.training_adversary.perturb(x, logits.detach())
-                else:
-                    xadv = self.training_adversary.perturb(x, y)    
-                model = model.train(True)
-                model.requires_grad_(True)
-            else:
-                xadv = self.test_adversary.perturb(x, y)
-        
-        adv_logits, interm_Zadv = model(xadv, store_intermediate=True)        
-        
-        if self.args.detach_adv_logits:
-            C = self.model.layers[-2].requires_grad_(False)
-            adv_logits = C(interm_Zadv[-1])
-            C.requires_grad_(True)
-
-        y_ = (y + 1).unsqueeze(1).float()
-        label_mask = ((y_.mm(1/y_.transpose(0,1))) != 1).float()
-        label_mask[label_mask == 0] = -1
-        # label_mask = torch.triu(label_mask, 1)
-
-        zzadv_diff = []
-        zz_diff = []
-        for i, (z, zadv) in enumerate(zip(interm_Z, interm_Zadv)):           
-            if self.args.z_criterion == 'diff':
-                _z_diff = z-zadv
-                zzadv_diff.append(_z_diff.view(z.shape[0], -1))
-            elif self.args.z_criterion == 'diff-spread':
-                _z_diff = z-zadv
-                
-                z = z.view(z.shape[0], -1)
-                pw_dist = (z.unsqueeze(1) - z.unsqueeze(0)).view(-1, z.shape[1])
-                pw_dist_std = torch.std(pw_dist, dim=0).detach()
-
-                _z_diff = _z_diff.view(z.shape[0], -1)                
-                _z_diff = _z_diff/pw_dist_std
-
-                zzadv_diff.append(_z_diff)
-            elif self.args.z_criterion == 'cosine':
-                z = z.view(z.shape[0], -1)
-                zadv = zadv.view(z.shape[0], -1)
-                _z_diff = 1 - nn.functional.cosine_similarity(z, zadv, dim=1)
-                zzadv_diff.append(_z_diff.view(z.shape[0], -1))
-            elif self.args.z_criterion == 'cosine-spread':
-                z = z.view(z.shape[0], -1)
-                zadv = zadv.view(z.shape[0], -1)
-
-                normed_z = z / torch.norm(z, p=2, dim=1, keepdim=True)
-                normed_zadv = zadv / torch.norm(zadv, p=2, dim=1, keepdim=True)
-                
-                zz_cos = z.mm(z.transpose(0,1)) * label_mask                
-                zz_cos[label_mask == -1] += 1
-                zz_cos = torch.abs(zz_cos)                
-                zz_cos = (zz_cos).sum(1, keepdim=True) / ((label_mask != 0).float().sum(1, keepdim=True)*2)
-                zzadv_cos = 1 - nn.functional.cosine_similarity(z, zadv, dim=1).unsqueeze(1)                
-                zz_diff.append(zz_cos)
-                zzadv_diff.append(zzadv_cos)
-        if len(zz_diff) > 0:
-            zz_diff = torch.cat(zz_diff, dim=1)
-        zzadv_diff = torch.cat(zzadv_diff, dim=1)
-        return xadv, logits, adv_logits, zzadv_diff, zz_diff
-
-    def train_step(self, batch, batch_idx):
-        x,y = batch
-        x = x.to(self.device)
-        y = y.to(self.device)
-        
-        xadv, logits, adv_logits, z_diff, zz_diff = self.compute_outputs(x, y)
-
-        cln_acc, _ = compute_accuracy(logits, y)
-        adv_acc, _ = compute_accuracy(adv_logits, y)
-
-        if self.model.training and self.args.adv_ratio < 1:
-            selected_adv = np.random.binomial(1, p=self.args.adv_ratio, size=len(logits)).astype(bool)
-            while selected_adv.all() or (not selected_adv.any()):
-                selected_adv = np.random.binomial(1, p=self.args.adv_ratio, size=len(logits)).astype(bool)
-            selected_cln = (~ selected_adv)
-        else:
-            selected_adv = selected_cln = np.ones((len(logits,))).astype(bool)
-        loss = cln_classification_loss = torch.nn.functional.cross_entropy(logits[selected_cln], y[selected_cln])        
-        if self.args.adv_loss_wt > 0:
-            if self.args.maximize_logit_divergence:
-                adv_classification_loss = self.training_adversary.loss_fn(adv_logits[selected_adv], logits[selected_adv])
-            else:
-                adv_classification_loss = self.training_adversary.loss_fn(adv_logits[selected_adv], y[selected_adv])
-            loss += self.args.adv_loss_wt*adv_classification_loss        
-        z_diff_norm = torch.norm(z_diff, p=2, dim=1)**2
-        if len(zz_diff) > 0:
-            zz_diff_norm = zz_diff.pow(2).sum(1)
-        else:
-            zz_diff_norm = 0
-        if self.model.training and self.args.use_MoM:
-            if len(zz_diff) > 0:
-                z_diff = torch.cat((z_diff, zz_diff), dim=1)
-            if isinstance(self.optimizer.L, int):
-                self.optimizer.L = torch.zeros((z_diff.shape[1],1), device=z_diff.device) + self.optimizer.L
-            z_term = z_diff.mm(self.optimizer.L).squeeze() + (self.optimizer.c * (z_diff_norm + zz_diff_norm))/2            
-        elif self.args.z_wt > 0:
-            if self.args.z_criterion == 'cosine-spread':                
-                zz_diff_norm = zz_diff.sum(1)
-                z_diff_norm = z_diff.sum(1)
-                z_term = self.args.z_wt*z_diff_norm + self.args.zz_wt*zz_diff_norm
-            else:
-                z_term = self.args.z_wt*(z_diff_norm)
-        else:
-            z_term = 0
-        loss += z_term.mean() if isinstance(z_term, torch.Tensor) else 0        
-        if self.model.training and self.args.use_MoM:
-            self.optimizer.zero_grad()
-            loss.backward()
-            nn.utils.clip_grad_norm_(self.model.parameters(), 5)
-            self.optimizer.step()
-            with torch.no_grad():
-                _, _, _, z_diff, zz_diff = self.compute_outputs(x, y, xadv)
-                if len(zz_diff) > 0:
-                    z_diff = torch.cat((z_diff, zz_diff), dim=1)
-                self.optimizer.L += self.optimizer.c * z_diff.mean(0).unsqueeze(1)
-            self.optimizer.c *= self.args.c_step_size
-            loss.detach()            
-        return {'loss':loss}, {'train_clean_accuracy': cln_acc,
-                             'train_adv_accuracy': adv_acc,
-                             'train_accuracy': (cln_acc + adv_acc) / 2,
-                             'train_loss': float(loss.detach().cpu()),
-                             'train_Z_loss': float(z_diff_norm.max().detach().cpu()),
-                             'train_ZZ_loss': float(zz_diff_norm.max().detach().cpu()) if len(zz_diff) > 0 else 0}
-    def _optimization_wrapper(self, func):        
-        if self.args.use_MoM:
-            return func
-        else:
-            def wrapper(*args, **kwargs):
-                self.optimizer.zero_grad()
-                output, logs = func(*args, **kwargs)
-                output['loss'].backward()
-                nn.utils.clip_grad_norm_(self.model.parameters(), 5)
-                self.optimizer.step()
-                return output, logs
-            return wrapper    
+  
 
 class TRADESTrainer(Trainer):
     def __init__(self, model, train_loader, val_loader, test_loader, optimizer, scheduler, device, args):
         super().__init__(model, train_loader, val_loader, test_loader, optimizer, scheduler, device, args)
         attack_class, kwargs = extract_attack(args)
         self.adversary = attack_class(self.model, **kwargs)
+
+    def compute_adversarial(self, x, y):
+        model = self.model
+        model.requires_grad_(False)
+        if model.training:            
+            model = model.train(False)
+            xadv = self.adversary.perturb(x, y)    
+            model = model.train(True)            
+        else:
+            xadv = self.adversary.perturb(x, y)
+        model.requires_grad_(True)
+        return xadv
+
     def train_step(self, batch, batch_idx):
         x,y = batch
         x = x.to(self.device)
@@ -184,7 +40,7 @@ class TRADESTrainer(Trainer):
         if self.model.training:
             loss, logits, adv_logits = trades_loss(self.model, x, y, self.optimizer, self.args.eps_iter, self.args.eps, self.args.nb_iter, self.args.adv_loss_wt)
         else:
-            xadv = self.adversary.perturb(x, y)
+            xadv = self.compute_adversarial(x, y)
             logits = self.model(x)
             adv_logits = self.model(xadv)
             loss = torch.nn.functional.cross_entropy(logits, y)
@@ -192,7 +48,9 @@ class TRADESTrainer(Trainer):
         
         cln_acc, _ = compute_accuracy(logits, y)
         adv_acc, _ = compute_accuracy(adv_logits, y)
-        
+        if self.model.training:
+            for p in self.model.parameters():
+                assert (p.grad is None) or (p.grad == 0).all()
         return {'loss': loss}, {'train_clean_accuracy': cln_acc,
                              'train_adv_accuracy': adv_acc,
                              'train_accuracy': (cln_acc + adv_acc) / 2,
@@ -204,18 +62,6 @@ def add_gaussian_smoothing_layers(model:LayeredModel):
             GaussianNoiseLayer(),
             l
         )
-
-class AdamMoM(torch.optim.Adam):
-    def __init__(self, model, init_c, lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False):
-        super().__init__(model.parameters(), lr, betas, eps, weight_decay, amsgrad)
-        self.c = init_c        
-        self.L = 0       
-
-class SGDMoM(torch.optim.SGD):
-    def __init__(self, model, init_c, lr, momentum=0, dampening=0, weight_decay=0, nesterov=False):
-        super().__init__(model.parameters(), lr, momentum, dampening, weight_decay, nesterov)
-        self.c = init_c
-        self.L = 0
 def train(args):
     train_dataset, val_dataset, test_dataset, num_classes = get_cifar10_dataset(args.datafolder)
     # train_dataset = [train_dataset[i] for i in range(10000)]
@@ -231,8 +77,14 @@ def train(args):
         'wide_resnet-10': lambda : WideResnet(args, 28, 10, 0, num_classes)
     }
     model = model_dict[args.model_name]().to(device)
+    # print(model)
     if len(args.layer_idxs) == 0:
-        model.args.layer_idxs = torch.arange(len(model.layers))
+        model.args.layer_idxs = list(range(len(model.layers) - 1))
+        if args.include_logits:
+            model.args.layer_idxs.append(len(model.layers)-1)
+    model.args.layer_idxs = torch.tensor(model.args.layer_idxs)
+    print(model.args.layer_idxs)
+
     if args.optimizer == 'adam':
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.0005)
     elif args.optimizer == 'sgd':
@@ -279,7 +131,8 @@ def test(args):
 
     args.logdir = os.path.join(*([x for x in os.path.dirname(args.model_path).split('/') if x != 'checkpoints']))
     print(args.logdir)
-    
+    args.use_MoM = 'MoM' in args.optimizer
+
     trainer = ActivationInvarianceTrainer(model, None, None, test_loader, None, None, device, args)
     trainer.track_metric('train_accuracy', 'max')
     trainer.test()
@@ -295,8 +148,8 @@ if __name__ == '__main__':
     parser.add_argument('--model_path', default='')
     parser.add_argument('--normalize_input', action='store_true')
 
-    parser.add_argument('--logdir', default='new_logs/activation_invariance/')
-    parser.add_argument('--exp_name', default='')
+    parser.add_argument('--logdir', default='logs/activation_invariance/')
+    parser.add_argument('--exp_name', default='training')
     
     parser.add_argument('--nepochs', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=128)
@@ -319,21 +172,31 @@ if __name__ == '__main__':
     parser.add_argument('--max_iters', type=int, default=7)
     parser.add_argument('--std', type=float, default=0.25)
     parser.add_argument('--maximize_logit_divergence', action='store_true')
+    parser.add_argument('--T', type=float, default=1.)
+    parser.add_argument('--adv_loss_fn', type=str, choices=('xent', 'z_cos'), default='xent')
+    parser.add_argument('--accumulate_adv_grad_prob', type=float, default=0.)
 
     parser.add_argument('--z_wt', type=float, default=1e-3)
     parser.add_argument('--zz_wt', type=float, default=1e-3)
     parser.add_argument('--adv_loss_wt', type=float, default=1)
+    parser.add_argument('--cln_loss_wt', type=float, default=1)
     parser.add_argument('--adv_ratio', type=float, default=1.)
     parser.add_argument('--detach_adv_logits', action='store_true')
     parser.add_argument('--layer_idxs', type=int, nargs='+', default=[])
+    parser.add_argument('--include_logits', action='store_true')
     parser.add_argument('--z_criterion', type=str, default='diff')
+    parser.add_argument('--use_preactivation', action='store_true')
 
-    parser.add_argument('--regress_on_logits', action='store_true')
+    parser.add_argument('--match_logits', action='store_true')
+    parser.add_argument('--logit_matching_fn', type=str, choices=('KL', 'cosine', 'L2'), default='KL')
+    parser.add_argument('--logit_loss_wt', type=float, default=1)
     parser.add_argument('--normalize_activations', action='store_true')
 
     parser.add_argument('--random_seed', type=int, default=9999)
 
     parser.add_argument('--TRADES', action='store_true')
+
+    parser.add_argument('--debug', action='store_true')
 
     np.random.seed(9999)
     torch.random.manual_seed(9999)
